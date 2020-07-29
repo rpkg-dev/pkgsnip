d824454568cfbc6503c642de8d14065076506e20
Salim B <salim@posteo.de>
2020-07-29 23:00:33 +0200

    minor change

--- tests/testthat/test-pkgsnip.R
+++ tests/testthat/test-pkgsnip.R
@@ -25,7 +25,7 @@ test_that("All roxygen2 tag labels are unique", {
 test_that("All common abbreviations are unique", {
 
   expect_false(abbreviations() %>%
-                 purrr::map(identity) %>%
+                 as.list() %>%
                  purrr::map_lgl(~ any(duplicated(.x))) %>%
                  any())
 })
